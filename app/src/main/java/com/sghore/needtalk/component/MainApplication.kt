1e35499855e5e7b54383e4e9721e704502052734
sghore <sghoregooteitehoo03@gmail.com>
2024-01-04 17:01:08 +0900

    feat: init commit - 라이브러리 추가 - Hilt 기본 세팅

---
+++ app/src/main/java/com/sghore/needtalk/component/MainApplication.kt
@@ -0,0 +1,8 @@
+package com.sghore.needtalk.component
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+}
---
+++ app/src/main/java/com/sghore/needtalk/presentation/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.sghore.needtalk.presentation.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color.White,
+    primaryVariant = Color.White,
+    onPrimary = Color.Black,
+    secondary = Orange,
+    secondaryVariant = Orange
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun NeedTalkTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        content = content
+    )
+}
