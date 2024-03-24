bdef9dd7c3b8ef490792ba65c711922d4d17feae
sghore <sghoregooteitehoo03@gmail.com>
2024-03-24 22:45:46 +0900

    fix: 버전코드 및 버전이름 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sghore.needtalk"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
